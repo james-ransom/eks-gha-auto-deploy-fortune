3e0751191542f67bef54c318adfab9bead051343
james ransom <43888908+james-ransom@users.noreply.github.com>
2022-12-06 15:30:01 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
 </head>
 <div class="black-block" style="margin-top:10%;background:black;padding-top:10%;padding-bottom:10%">
   <div class="container">
-    <p style="text-transform:uppercase;margin-bottom:-2%;font-weight:800;color:#3186d2">FORTUNE</p>
+    <p style="text-transform:uppercase;margin-bottom:20%;font-weight:800;color:#3186d2">FORTUNE</p>
     <h2 style="color:#fff"><?php echo `fortune`; ?></h2>        
   </div>
 </div>
